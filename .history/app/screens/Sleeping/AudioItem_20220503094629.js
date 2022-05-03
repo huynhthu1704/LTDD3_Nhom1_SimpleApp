1507cbcf2a5ad2f404bd0827f705c6734045c49b
thuhtn2001 <thu.htn2001@gmail.com>
2022-05-03 13:17:26 +0700

    add event for catedory + header bar

---
+++ .history/app/screens/Sleeping/AudioItem_20220503094629.js
@@ -0,0 +1,43 @@
+import {
+    StyleSheet,
+    Text,
+    View,
+    ScrollView,
+    Image,
+    FlatList,
+    SafeAreaView,
+    TouchableOpacity,
+    TouchableWithoutFeedback,
+  } from "react-native";
+  import React, { useState } from "react";
+  import { appTheme, COLORS, FONTS, SIZES, images } from "../../constants/index";
+import { NavigationContainer } from "@react-navigation/native";
+
+export default function AudioItem({ item, navigation }) {
+    return (
+      <TouchableOpacity
+        style={{
+          borderRadius: 10,
+          paddingBottom: SIZES.padding / 3,
+          margin: SIZES.padding /4,
+         // justifyContent: "center",
+          alignItems: "center",
+          width: 100,
+          // backgroundColor : isSelected? COLORS.darkPurple : COLORS.white2,
+        }}
+        onPress={() => navigation.navigate("PlayAudio", {id : item.id})}
+      >
+        <Image
+          style={{ width: "100%", height: 100, borderRadius: 10 }}
+          source={{
+            uri: "http://khoack.tdc.edu.vn/wp-content/uploads/2017/01/Logo-Cao-dang1.jpg",
+          }}
+        />
+        <View>
+            <Text style={{ ...FONTS.h4, color: COLORS.white }}>{item.name}</Text>
+        <Text style={{ ...FONTS.body4, color: COLORS.white }}>{item.author}</Text>
+        </View>
+      
+      </TouchableOpacity>
+    );
+  }
---
+++ .history/app/screens/Sleeping/PlayAudio_20220503053520.js
@@ -0,0 +1,196 @@
+import {
+  StyleSheet,
+  Text,
+  View,
+  ImageBackground,
+  TouchableWithoutFeedback,
+} from "react-native";
+import React, { useState, useEffect } from "react";
+import HeaderBar from "../../components/HeaderBar";
+import { SIZES, COLORS, FONTS } from "../../constants/index";
+import data from "../../constants/data";
+import { backgroundColor } from "react-native/Libraries/Components/View/ReactNativeStyleAttributes";
+import { FontAwesome, MaterialIcons } from "@expo/vector-icons";
+import Slider from "@react-native-community/slider";
+import { Audio } from "expo-av";
+export default function PlayAudio({ route }) {
+  const audio = data.audio.find((item) => item.id == route.params.id);
+  const hour = Math.floor(audio.duration / 3600);
+  const minute = Math.floor((audio.duration - hour * 3600) / 60);
+  const sec = audio.duration - minute * 60;
+  const [isPlaying, setPlay] = useState(false);
+  const [playTime, setPlayTime] = useState(0);
+  const [playHour, setPlayHour] = useState(0);
+  const [playMin, setPlayMin] = useState(0);
+  const [playSec, setPlaySec] = useState(0);
+  const [like, setLike] = useState(false);
+  const [sound, setSound] = React.useState();
+
+  async function playSound() {
+    console.log("Loadingggg Sound");
+    const { sound } = await Audio.Sound.createAsync(audio.src);
+    setSound(sound);
+    // Audio.setAudioModeAsync({ staysActiveInBackground: true });
+    console.log("Playing Sound");
+    await sound.playAsync();
+  }
+
+  React.useEffect(() => {
+    return sound
+      ? () => {
+          console.log("Unloading Sound");
+          sound.unloadAsync();
+        }
+      : undefined;
+  }, [sound]);
+  return (
+    <View style={{ backgroundColor: "red", flex: 1 }}>
+      <ImageBackground
+        style={{ width: "100%", height: "100%" }}
+        source={audio.img}
+      >
+        {/* Header bar */}
+        <HeaderBar
+          colors={["transparent", "transparent"]}
+          color={COLORS.white}
+          leftIcon="chevron-down"
+          rightIcon="heart"
+        />
+        <View style={{ flex: 1 }}>
+          <View
+            style={{ flex: 2, justifyContent: "center", alignItems: "center" }}
+          >
+            <Text style={{ ...FONTS.h1, color: COLORS.white }}>
+              {audio.name}
+            </Text>
+            <Text style={{ ...FONTS.body2, color: COLORS.white }}>
+              {audio.author}
+            </Text>
+            {/* Duration */}
+            <View
+              style={{
+                backgroundColor: "rgba(0, 0, 0, 0.4)",
+                borderRadius: 50,
+                padding: SIZES.padding / 2,
+              }}
+            >
+              <Text style={{ color: COLORS.white }}>
+                {hour > 0 ? hour + " : " : ""}
+                {minute > 10 ? minute : "0" + minute} :
+                {sec > 10 ? sec : "0" + sec}
+              </Text>
+            </View>
+            {/* Replay, play and forward button */}
+            <View style={{ flexDirection: "row", alignItems: "center" }}>
+              <TouchableWithoutFeedback
+                onPress={() => setPlayTime(playTime - 30)}
+                style={{
+                  borderRadius: 50,
+                  padding: 20,
+                  marginTop: 10,
+                }}
+              >
+                <MaterialIcons
+                  style={{ fontSize: 35, color: COLORS.white }}
+                  name="replay-30"
+                />
+              </TouchableWithoutFeedback>
+              <View style={{ marginHorizontal: 10 }}>
+                <TouchableWithoutFeedback
+                  onPress={() => {
+                    setPlay(!isPlaying);
+                    playSound();
+                  }}
+                  style={{
+                    borderRadius: 50,
+                    padding: 20,
+                    marginTop: 10,
+                  }}
+                >
+                  <FontAwesome
+                    style={{
+                      fontSize: 50,
+                      color: COLORS.white,
+                      display: isPlaying ? "flex" : "none",
+                    }}
+                    name="pause"
+                  />
+                </TouchableWithoutFeedback>
+                <TouchableWithoutFeedback
+                  onPress={() => setPlay(!isPlaying)}
+                  style={{
+                    borderRadius: 50,
+                    padding: 20,
+                    marginTop: 10,
+                  }}
+                >
+                  <FontAwesome
+                    style={{
+                      fontSize: 50,
+                      color: COLORS.white,
+                      display: !isPlaying ? "flex" : "none",
+                    }}
+                    name="play"
+                  />
+                </TouchableWithoutFeedback>
+              </View>
+              <TouchableWithoutFeedback
+                onPress={() => setPlayTime(playTime + 30)}
+                style={{
+                  borderRadius: 50,
+                  padding: 20,
+                  marginTop: 10,
+                }}
+              >
+                <MaterialIcons
+                  style={{ fontSize: 35, color: COLORS.white }}
+                  name="forward-30"
+                />
+              </TouchableWithoutFeedback>
+            </View>
+          </View>
+          <View style={{ flex: 1, justifyContent: "center" }}>
+            {/* Slider */}
+            <Slider
+              style={{ width: "100%", height: 20 }}
+              value={playTime}
+              minimumValue={0}
+              thumbTintColor="red"
+              minimumTrackTintColor="red"
+              maximumValue={audio.duration}
+              onValueChange={(value) => {
+                setPlayTime(value);
+                console.log(playTime);
+                setPlayHour(Math.floor(playTime / 3600));
+                setPlayMin(Math.floor((playTime - playHour * 3600) / 60));
+                console.log(playMin);
+                setPlaySec(Math.round(playTime - playMin * 60));
+                console.log(playSec);
+              }}
+            />
+            <View
+              style={{
+                paddingHorizontal: SIZES.padding / 2,
+                flexDirection: "row",
+                justifyContent: "space-between",
+              }}
+            >
+              <Text style={{ color: COLORS.white }}>
+                {playHour > 0 ? playHour + " : " : ""}
+                {playMin >= 10 ? playMin : "0" + playMin} :
+                {playSec >= 10 ? playSec : "0" + playSec}
+              </Text>
+              <Text style={{ color: COLORS.white }}>
+                {hour > 0 ? hour + " : " : ""}
+                {minute >= 10 ? minute : "0" + minute} :
+                {sec >= 10 ? sec : "0" + sec}
+              </Text>
+            </View>
+          </View>
+        </View>
+      </ImageBackground>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
